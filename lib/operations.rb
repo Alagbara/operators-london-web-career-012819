9aa4f51f63ceaed2015b35e48b45f1a7e505e66c
Moses Olawoyin <m.bastford@gmail.com>
2019-01-20 22:04:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+if speed == 30 && speed == 95
+  return "false"
+  else 
+    return "true"
 end
 
 
 
 def not_safe?(speed)
-	
+  speed == 30 && speed == 95 ? "not safe" : "safe"
+  
 end
 	
 
